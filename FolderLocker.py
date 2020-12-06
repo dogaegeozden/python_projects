68d4c873e4d1d48dae4515d2b93df46b82a154ac
dogaege <61098483+dogaege@users.noreply.github.com>
2020-12-06 03:21:48 -0800

    Update FolderLocker.py

--- FolderLocker.py
+++ FolderLocker.py
@@ -1,25 +1,21 @@
-# For general security you should avoid words such as personal files when
-# you are hiding your files because they can be found by those key
-# words.
-# !! Note: If you wanna use this folderlocker on a mac or any linux based operating system
-# you should change the todir='path' to your desktop path or your documents path.
-# ex: '/home/<user>/Desktop'
-
 import os
 import shutil
+import tkinter as tk
+from tkinter import filedialog
 
-print("This application designed for Windows Operating Systems")
-from_dir=input('ENTER the absoulute path of folder to protect(Note: You should change the backward slashs to forward slashs. You can find the full path by right clicking on your folder inside security tab): ')
-fname=input('Enter folder name of create: ')
-caracterlist = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "!", "@", "#", "$", "%", "^", "&", "(", ")", "-", "_", "=", "+", ",", ";", "[", "]" ]
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askdirectory()
+from_dir= file_path
+fname=input('Enter folder name to be created: ')
+caracterlist = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "!", "@", "#", "$", "%", "^", "&", "(", ")", "-", "_", "=", "+", ",", ";", "[", "]", "{", "}" ]
 caracterstring = ""
 for c in caracterlist:
     s = str(c)
     caracterstring += s + ", "
 
-pasw=input("Enter the password(Note: You can't use all the caracters such as capital letters and some of the symbols here is list of the caracters that you can use {}.): ".format(caracterstring))
-
-print('This script will create a the folder you named in your desktop.')
+pasw=input("Enter a password(Note: You can't use all the caracters such as capital letters and some of the symbols here is list of the caracters that you can use {}.): ".format(caracterstring))
 
 user = os.getcwd().split("\\")[2]
 todir='C:/Users/{}/Desktop'.format(user)
@@ -48,4 +44,5 @@ try:
 except:
     print()
 
-print("Encryption is done!, You can find your encrypted folder on your desktop.")
+print("Encryption is done!")
+print("You can find the encrypted folder in your desktop.")
