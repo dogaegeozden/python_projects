e190381b3d61332bca984ad7e19a1b5042c57032
Doga <61098483+dogaegeozden@users.noreply.github.com>
2021-08-22 15:30:57 -0700

    Update youtube_downloader.py

--- youtube_downloader/youtube_downloader.py
+++ youtube_downloader/youtube_downloader.py
@@ -23,14 +23,32 @@ from pytube import YouTube
 
 while True:
     try:
-        question1 = str(input("Would you like to download as video or audio? (v/a)"))
+        question1 = str(input("Would you like to download as video or audio? (v/a): "))
         question2 = input("Enter you the url here: ")
         if question1 == "v":
             yt = YouTube(f'{question2}')
             yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+            print(yt.title + " has been successfully downloaded.")
 
-        if question1 == "a":
+        elif question1 == "a":
+            # url input from user
             yt = YouTube(f'{question2}')
-            yt.streams.filter(only_audio=True).first().download()
+
+            # extract only audio
+            video = yt.streams.filter(only_audio=True).first()
+
+            destination = os.getcwd()
+
+            # download the file
+            out_file = video.download(output_path=destination)
+
+            # save the file
+            base, ext = os.path.splitext(out_file)
+            new_file = base + '.mp3'
+            os.rename(out_file, new_file)
+
+            # result of success
+            print(yt.title + " has been successfully downloaded.")
+
     except:
         print("You didn't entered a valid value. You should be entering a url as fallows: https://www.youtube.com/watch?v=htrxWYQayk0")
