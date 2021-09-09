159ef7d674f9a56e001502b7202ee290f1191475
Doga <61098483+dogaegeozden@users.noreply.github.com>
2021-09-09 16:40:47 -0700

    Add files via upload

---
+++ total_sales_in_years_line_graph/total_sales_in_years_line_graph.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+y = np.array([300, 310, 340, 320, 340, 415, 420, 390, 455, 445, 460])
+x = np.array([2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020])
+
+plt.title("Revenue")
+plt.ylabel("Total Sales")
+plt.xlabel("Years")
+
+plt.plot(x, y)
+
+plt.grid()
+
+plt.show()
